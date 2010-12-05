e30219ecbe9ea625c625a9e4ab78091d790645cf
Vanwaril <manojm@ieee.org>
2010-12-05 15:58:56 +0530

    domify working, tested. New Dependencies: jsdom, htmlparser

--- src/libraries/contentgrabber.js
+++ src/libraries/contentgrabber.js
@@ -1,9 +1,21 @@
+
+/* Dependencies */
 var Readability = require('./external/readability.js');
+var jsdom       = require('jsdom');
+
+/*
+ * ContentGrabber: given a URL, fetch *only* the text
+ * content from that page.
+ */
 
 var ContentGrabber = function() {
 
-    thisContentGrabber = this;
-
+	thisContentGrabber = this;
+	
+	this.domify = function(html)
+	{
+		return jsdom.jsdom(html);
+	};
 };
 
 module.exports = new ContentGrabber();
--- test/libraries/contentgrabber.js
+++ test/libraries/contentgrabber.js
@@ -1,33 +1,42 @@
+
 /*
  *  Module dependencies
  */
-
 var nodeunit = require('nodeunit');
 var ContentGrabber = require('../../src/libraries/contentgrabber.js');
 
 /*
  *  Sample data
  */
+var html='<html><head></head><body><div id="lol">hello</div></body></html>';
 
 /*
  *  Tests
  */
-
-exports['grab content from page'] = nodeunit.testCase({
-
+exports['grab content from page'] = nodeunit.testCase(
+{
 /*    setUp: function () {
     },
  
     tearDown: function () {
     },
  */
+	'basic': function(test) {
+		test.expect(1);
+		test.equal(1, 1);
+		test.done();
+	}
+});
 
-    'basic': function(test) {
-        
-        test.expect(1);
-        test.equal(1, 1);
-        test.done();
-
-    }
-
+exports['DOM Testing'] = nodeunit.testCase(
+{
+	'basic': function(test)
+	{
+		test.expect(2);
+		var dom = ContentGrabber.domify(html);
+		test.equal(dom.getElementById('lol').innerHTML, 'hello');
+		dom.getElementById('lol').innerHTML = "jigglypuff!";
+		test.equal(dom.getElementById('lol').innerHTML, 'jigglypuff!');
+		test.done();
+	}
 });
