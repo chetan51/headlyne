215304335bdaec662d968ec21b444f4827ffc6ed
Vanwaril <manojm@ieee.org>
2010-12-05 19:49:54 +0530

    Feed Parser re-factored to be more modular, follow coding conventions.

--- src/libraries/feedparser.js
+++ src/libraries/feedparser.js
@@ -1,8 +1,20 @@
+/**
+ * Dependencies
+ **/
 var xml = require('node-xml');
 
-var FeedParser = function() {
+
+/**
+ * FeedParser: object that handles everything from
+ * taking a string representing a feed and providing
+ * access to all necessary parts of it.
+ **/
+
+var FeedParser = function()
+{
 	
-	/* parse(rss, callback):
+	/**
+	 * parse(rss, callback):
 	 * Parses a string in 'rss' into a set of objects.
 	 * Callback = function(channelinfo, items, type, version):
 	 * channelinfo: array of [key, value] pairs.
@@ -17,10 +29,10 @@ var FeedParser = function() {
 	 *
 	 * Type: type of the feed. ('atom' or 'rss')
 	 * Version: 0 if the version cannot be detected. Otherwise the version number.
-	 */
+	 **/
 	
-	
-	/* Example Usage:
+	/**
+	 * Example Usage:
 	 * FeedParser.parse(string,
 	 *      function(channelinfo, items, type, version)
 	 *      {
@@ -33,157 +45,301 @@ var FeedParser = function() {
 	 *      	}
 	 *      }
 	 * );
-	 */
+	 **/
 
 	this.parse = function(rss, callback, errback, timeout)
 	{
-		var items=[];		// The items returned via the callback.
-		var channelinfo=[];	// The channel information returned via the callback.
+		var items=[];           // the items returned via the callback.
+		var channelinfo=[];     // the channel information returned via the callback.
 		
-		var cur_item=0;		// The number of the current item.
-		var type='';		// Type of feed: rss or atom.0.91, rss 0.92, rss 2.0, feed xmlns="http://www.w3.org/2005/Atom"
-		var version='';		// Version: 0.91, 0.92, 2.0, or 1.0. Undefined: 0.
+		var cur_item=0;         // the number of the current item.
+		var type='';            // type of feed: rss or atom.
+		var version='';         // version: 0.91, 0.92, 2.0, or 1.0. Undefined: 0.
 		
-		var tagstack=[];	// A stack of tags the current tag is in.
+		var tagstack=[];        // a stack of tags the current tag is in.
 		
 		var content='';
-		var rellink;		// Set to true ONLY if link is rel=alternate, for atom.
-		var pureswitch;		// With atom, if type = 'html' or 'xhtml' for an element, it can contain markup.
-					// If pureswitch=x, then until the matching close tag (at depth x), add everything to content.
-		var result=false;	// set to true on end document/error. If no result after time out, return error.
+		var rellink;            // set to true ONLY if atom link is rel=alternate
 
+		var pureswitch;         // with atom, if type = 'html' or 'xhtml' for an
+		                        // element, it can contain markup. if pureswitch=x,
+		                        // then until the matching close tag (at depth x)
+		                        // add everything to content.
 
-		var parser = new xml.SaxParser(function(cb) {
-			cb.onStartDocument(function() {
-				tagstack.push('root');
-				pureswitch=0;
-				rellink=false;
-				items[0] = [];
-			});
+		var result=false;       // set to true on end document/error.
+		                        // if no result after time out, return error.
 
-			cb.onEndDocument(function() {
-				if(!result) {
-					items.pop();
-					result=true;
-					callback(channelinfo, items, type, version);
+		/**
+		 * This is the xml parser from node-xml.
+		 * We define callback functions that are triggered
+		 * when the corresponding event occurs.
+		 **/
+		var parser = new xml.SaxParser(function(cb)
+		{
+			/**
+			 * Initialize parser
+			 **/
+			cb.onStartDocument(
+				function() {
+					tagstack.push('root');
+					pureswitch=0;
+					rellink=false;
+					items[0] = [];
 				}
-			});
+			);
+
+			cb.onEndDocument(
+				function() {
+					if(!result) {
+						items.pop();
+						result=true;
+						callback(channelinfo, items, type, version);
+					}
+				}
+			);
 			
-			cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
-				if(pureswitch) // add the tag as the content, NOT xml.
+			cb.onError(
+				function(err) {
+					if(!result) {
+						result=true;
+						errback(new Error(err));
+					}
+				}
+			);
+
+			/**
+			 * textifyOpenTag(elem, prefix, attrs)
+			 * 
+			 * Add <prefix:elem attr[i][0]=attr[i][1] ...>
+			 * to the content block.
+			 **/
+			function textifyOpenTag(elem, prefix, attrs)
+			{
+				content +=
+					"<"     +
+					prefix  +
+					":"     +
+					elem    +
+					" ";
+				
+				for(i in attrs)
 				{
-					content+="<"+prefix+":"+elem+" ";
-					for(i in attrs)
-						content+=attrs[i][0]+"="+attrs[i][1]+" ";
-					content+=">";
-					
-				} else {
-					for(i in attrs) {
-						if(attrs[i][0] == 'type')
-						{
-							if(attrs[i][1] == 'html' || attrs[i][1] == 'xhtml')
-							{
-								pureswitch = tagstack.length;
-							}
+					content +=
+						attrs[i][0] +
+						"="         +
+						attrs[i][1] +
+						" ";
+				}
+				content += ">";
+			}
+
+			/**
+			 * textifyCloseTag(elem, prefix)
+			 * 
+			 * Add </prefix:elem> to the content block.
+			 **/
+			function textifyCloseTag(elem, prefix)
+			{
+				content+="</"+prefix+":"+elem+">";
+			}
+
+			/**
+			 * switchPure(attrs)
+			 * 
+			 * If a tag has type='html' or 'xhtml' as an attribute,
+			 * set pureswitch to the tree-depth of that tag.
+			 **/
+			function switchPure(attrs)
+			{
+				for(i in attrs)
+				{
+					if(attrs[i][0] == 'type') {
+						if(attrs[i][1] == 'html' ||
+						   attrs[i][1] == 'xhtml') {
+							pureswitch = tagstack.length;
 						}
 					}
-					
-					switch(elem.toLowerCase())
-					{
-						case 'feed':                    // If I have opened a feed tag, get the xmlns to confirm as Atom
-							type='atom'; version=0;
-							for(i in attrs) {
-								if(attrs[i][0] == 'xmlns'){
-									if(attrs[i][1] == 'http://www.w3.org/2005/Atom'){
-										version=1;
-										break;
-									}
-								}
-							}
-							break;
-						case 'rss':                     // If I have opened a rss tag, get the version.
-							type='rss'; version=0;
-							for(i in attrs) {
-								if(attrs[i][0] == 'version') {
-									version=attrs[i][1];
-									break;
-								}
-							}
-							break;
-						case 'link':
-							for(i in attrs) {
-								if(attrs[i][0] == 'rel') {
-									if(attrs[i][1] == 'alternate') {
-										rellink=true;
-										break;
-									}
-								}
-							}
-							break;
-					}
-					content='';
 				}
-				tagstack.push(elem.toLowerCase());
-			}); // End cb.onStartElementNS
+			}
+
+			/**
+			 * getAtomVersion(attrs)
+			 * 
+			 * Simply checks an Atom open tag for its xmlns -- if it is
+			 * correct, sets the version to 1. Otherwise, defaults
+			 * to atom, version 0.
+			 **/
+			function getAtomVersion(attrs)
+			{
+				type='atom'; version=0;
+				for(i in attrs)
+				{
+					if(attrs[i][0] == 'xmlns') {
+						if(attrs[i][1] == 'http://www.w3.org/2005/Atom') {
+							version=1;
+							break;
+						}
+					}
+				}
+			}
 			
-			cb.onEndElementNS(function(elem, prefix, uri) {
-				if(pureswitch) {
-					content+="</"+prefix+":"+elem+">";
-				} else {
-					if(tagstack[tagstack.length-2] == 'channel' ||
-					        tagstack[tagstack.length-2] == 'feed') {
-						
-						switch(elem.toLowerCase())
-						{
-							case 'title':
-								channelinfo.push(['title', content]);
-								break;
-							case 'link':
-								if(type=='atom') {
-									if(rellink==true) {
-										channelinfo.push(['link',content]);
-										rellink=false;
-									}
-								} else	channelinfo.push(['link',content]);
-								
-								break;
-							case 'description':
-							case 'subtitle':
-								channelinfo.push(['description',content]);
-								break;
-						}
-					} else if(tagstack[tagstack.length -2] == 'item' ||
-					        tagstack[tagstack.length -2] == 'entry') {
-						
-						switch(elem.toLowerCase())
-						{
-							case 'title':
-								items[cur_item].push(['title',content]);
-								break;
-							case 'link':
-								if(type=='atom') {
-									if(rellink==true) {
-										items[cur_item].push(['link',content]);
-										rellink=false;
-									}
-								} else	items[cur_item].push(['link',content]);
-								break;
-							case 'content':
-							case 'description':
-								items[cur_item].push(['description',content]);
-								break;
-						}
-					} else { }  //its parent isn't the channel or an item, so we don't care.
-					
-					if(elem.toLowerCase() == 'item' || elem.toLowerCase() == 'entry') {
-						cur_item++; items[cur_item]=[];
+			/**
+			 * getRSSVersion(attrs)
+			 * 
+			 * Simply checks an RSS open tag for its version.
+			 * If it doesn't exist, it defaults to 0.
+			 **/
+			function getRSSVersion(attrs)
+			{
+				type='rss';
+				version=0;
+				for(i in attrs)
+				{
+					if(attrs[i][0] == 'version') {
+						version=attrs[i][1];
+						break;
 					}
-					
-					content='';
 				}
-				tagstack.pop();
-				if(tagstack.length == pureswitch) pureswitch = 0;
-			});
+			}
+			
+			/**
+			 * checkRelAlternate(attrs)
+			 * 
+			 * Checks for a rel attribute. If it exists and
+			 * is set to 'alternate', enables rellink=true.
+			 **/
+			function checkRelAlternate(attrs)
+			{
+				for(i in attrs)
+				{
+					if(attrs[i][0] == 'rel') {
+						if(attrs[i][1] == 'alternate') {
+							rellink=true;
+							break;
+						}
+					}
+				}
+			}
+			
+			/**
+			 * saveChannelInfo(elem)
+			 * 
+			 * Triggered when a close tag is encountered inside a
+			 * channel, save the content of this tag in channel_info.
+			 **/
+			function saveChannelInfo(elem)
+			{
+				switch(elem.toLowerCase())
+				{
+					case 'title':
+						channelinfo.push(['title', content]);
+						break;
+					case 'link':
+						if(type=='atom') {
+							if(rellink==true) {
+								channelinfo.push(['link',content]);
+								rellink=false;
+							}
+						} else	channelinfo.push(['link',content]);
+						
+						break;
+					case 'description':
+					case 'subtitle':
+						channelinfo.push(['description',content]);
+						break;
+				}
+			}
+			
+			/**
+			 * saveItemInfo(elem)
+			 * 
+			 * Triggered when a close tag is encountered inside an
+			 * item, save the content of this tag in the current item.
+			 **/
+			function saveItemInfo(elem)
+			{
+				switch(elem.toLowerCase())
+				{
+					case 'title':
+						items[cur_item].push(['title',content]);
+						break;
+					case 'link':
+						if(type=='atom') {
+							if(rellink==true) {
+								items[cur_item].push(['link',content]);
+								rellink=false;
+							}
+						} else	items[cur_item].push(['link',content]);
+						break;
+					case 'content':
+					case 'description':
+						items[cur_item].push(['description',content]);
+						break;
+				}
+			}
+
+			cb.onStartElementNS(
+				function(elem, attrs, prefix, uri, namespaces)
+				{
+					if(pureswitch) {
+						textifyOpenTag(elem, prefix, attrs);
+					} else {
+						// check if pureswitch should be enabled
+						switchPure(attrs);
+
+						// processing for some open tags
+						switch(elem.toLowerCase())
+						{
+							// If I have opened a feed tag, get the xmlns to confirm as Atom
+							case 'feed':
+								getAtomVersion(attrs);
+								break;
+
+							// If I have opened a rss tag, get the version.
+							case 'rss':
+								getRSSVersion(attrs);
+								break;
+							
+							// If I have opened a link, check if it has a rel=alternate.
+							case 'link':
+								checkRelAlternate(attrs);
+								break;
+						}
+						content='';
+					}
+					tagstack.push(elem.toLowerCase());
+				}
+			);
+
+			cb.onEndElementNS(
+				function(elem, prefix, uri)
+				{
+					if(pureswitch) {
+						textifyCloseTag(elem, prefix);
+					} else {
+						if(  tagstack[tagstack.length-2] == 'channel' ||
+						     tagstack[tagstack.length-2] == 'feed') {
+							saveChannelInfo(elem);
+						} else if(  tagstack[tagstack.length -2] == 'item' ||
+						            tagstack[tagstack.length -2] == 'entry') {
+							saveItemInfo(elem);
+						}
+
+						// if we are closing an item/entry, increment counter.
+						if(  elem.toLowerCase() == 'item' ||
+						     elem.toLowerCase() == 'entry') {
+							cur_item++; items[cur_item]=[];
+						}
+						content='';
+					}
+					tagstack.pop();
+
+					// if we are out of the pure block,
+					// reset the switch.
+					if(tagstack.length == pureswitch)
+						pureswitch = 0;
+				}
+			);
 			
 			cb.onCharacters(function(str) {
 				content+=str;
@@ -196,12 +352,6 @@ var FeedParser = function() {
 			cb.onWarning(function(warn) {
 			});
 			
-			cb.onError(function(err) {
-				if(!result) {
-					result=true;
-					errback(new Error(err));
-				}
-			});
 		}); // End xml.SAXParser creation.
 		
 		if(timeout == null || typeof(timeout) == 'undefined')
@@ -218,12 +368,12 @@ var FeedParser = function() {
 		
 	}   // End parse(url, callback, errback)
 
-	/* stripURLs(string): 
+	/**
+	 * stripURLs(string): 
 	 * Takes a string and returns an array of the URLs it contains.
 	 * 
 	 * Supports http/https protocols ONLY.
-	 *
-	 */
+	 **/
 	this.stripURLs = function(str)
 	{
 		geturl = new RegExp("(^|[ \t\r\n])((http|https):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))"
@@ -234,9 +384,8 @@ var FeedParser = function() {
 		
 		// Now, strip the leading ' ', tab, return or newline, if it exists
 		for(i in urls) {
-			if( urls[i][0] == ' ' || urls[i][0] == '\t' ||
-				urls[i][0] == '\r' || urls[i][0] == '\n')
-			{
+			if(  urls[i][0] == ' ' || urls[i][0] == '\t' ||
+			     urls[i][0] == '\r' || urls[i][0] == '\n') {
 				urls[i] = urls[i].slice(1);
 			}
 		}
