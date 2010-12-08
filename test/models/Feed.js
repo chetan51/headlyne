2f04ab2793143a44bc4b7a1ba66ac2e13d2d1963
Chetan Surpur <chetan51@gmail.com>
2010-12-07 18:02:21 -0800

    Added test file for feed model

---
+++ test/models/Feed.js
@@ -0,0 +1,12 @@
+var http = require('http')
+var nodeunit = require('nodeunit');
+var FeedModel = require('../../src/models/Feed.js');
+
+exports['save'] = nodeunit.testCase(
+{
+	'save 1 feed': function(test) {
+		test.expect(1);
+		test.ok();
+		test.done();
+	}
+});
