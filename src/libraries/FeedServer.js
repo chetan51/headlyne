8f7abef18de8c11f45df1776965e1a9425190eb4
Chetan Surpur <chetan51@gmail.com>
2010-12-12 19:34:54 -0800

    Added first test for FeedServer

--- src/libraries/FeedServer.js
+++ src/libraries/FeedServer.js
@@ -19,6 +19,31 @@ var FeedServer = function()
 {    
 	var thisFeedServer = this;
 	
+	/**
+	 *	Gets feed and items for feed for previewing to the user.
+	 *	
+	 *		If feed is in database and is up to date, calls callback
+	 *		with it immediately.
+	 *	
+	 *		If feed is not in database or is not up to date, and
+	 *		instant is false, it retrieves and stores the feed, and
+	 *		calls callback when the feed is ready.
+	 *	
+	 *		If feed is not in database or is not up to date, and
+	 *		instant is true, it calls the callback with null and
+	 *		retrieves and stores the feed in the background for
+	 *		later retrieval.
+	 *	
+	 *		Arguments: url of feed
+	 *		           number of feed items to return
+	 *		           callback function for success
+	 *		           callback function for error
+	 *		           return results instantly?
+	 **/
+	this.getFeedTeaser = function(url, num_feed_items, callback, errback, instant)
+	{
+		callback(null);
+	}
 };
 
 /**
--- test/libraries/FeedServer.js
+++ test/libraries/FeedServer.js
@@ -1,22 +1,56 @@
-var http       = require('http'),
-    nodeunit   = require('nodeunit'),
-    FeedServer = require('../../src/libraries/FeedServer.js');
+/**
+ *	Module dependencies
+ **/
+var http            = require('http'),
+    nodeunit        = require('nodeunit'),
+    ServerGenerator = require('../mocks/server.js');
+    FeedServer      = require('../../src/libraries/FeedServer.js');
 
+/**
+ *	Constants and mocks
+ **/
+var mock_server_host = "localhost",
+    mock_server_port = 7500;
+
+var basic_feed_url = "http://" +
+                     mock_server_host +
+                     ":" +
+                     mock_server_port +
+                     "/basic_feed";
+
+/**
+ *	Tests
+ **/
 exports['get feed teaser'] = nodeunit.testCase(
 {
 	
 	setUp: function(callback) {
-	       callback();
+		ServerGenerator.createServer(
+			mock_server_host,
+			mock_server_port,
+			function() {}
+		);
+		callback();
 	},
 	 
 	tearDown: function(callback) {
-	       callback();
+		callback();
 	},
 
-	'basic': function(test) {
+	'feed in not in database and instant is on': function(test) {
 		test.expect(1);
-		test.equals(true, true);
-		test.done();
+		
+		FeedServer.getFeedTeaser(
+			basic_feed_url,
+			10,
+			function(feed) {
+				test.equals(feed, null);
+				test.done();
+			},
+			function(err) {
+				test.done();
+			}
+		);
 	}
 
 });
