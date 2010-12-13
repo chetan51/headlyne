143ff871a5e13a06a7ad596657532b1fa856fd78
Chetan Surpur <chetan51@gmail.com>
2010-12-12 23:38:55 -0800

    Added more to second test case

--- test/libraries/FeedServer.js
+++ test/libraries/FeedServer.js
@@ -3,7 +3,8 @@
  **/
 var http            = require('http'),
     nodeunit        = require('nodeunit'),
-    ServerGenerator = require('../mocks/ServerGenerator.js');
+    ServerGenerator = require('../mocks/ServerGenerator.js'),
+    FeedModel       = require('../../src/models/Feed.js'),
     FeedServer      = require('../../src/libraries/FeedServer.js');
 
 /**
@@ -15,7 +16,7 @@ var mock_server      = null;
 
 var basic_feed_url   = "http://" + mock_server_host + ":" + mock_server_port
                      + "/basic_feed",
-    basic_feed_title = "Example Feed";
+    basic_feed_title = "RSS Title";
 
 /**
  *	Tests
@@ -61,14 +62,20 @@ exports['get feed teaser'] = nodeunit.testCase(
 	},
 
 	'feed in not in database and instant is off': function(test) {
-		test.expect(1);
+		test.expect(2);
 		
 		FeedServer.getFeedTeaser(
 			basic_feed_url,
 			10,
 			function(feed) {
 				test.equal(feed.title, basic_feed_title);
-				test.done();
+				FeedModel.get(
+					basic_feed_url,
+					function(feed) {
+						test.equal(feed.title, basic_feed_title);
+						test.done();
+					}
+				);
 			},
 			function(err) {
 				test.done();
