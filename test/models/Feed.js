8959d42bcc0919ceb4aa567bb554c9b6ad6fd628
Chetan Surpur <chetan51@gmail.com>
2010-12-12 23:54:47 -0800

    Moved database setup and teardown for tests to DatabaseMocker mock class

--- test/models/Feed.js
+++ test/models/Feed.js
@@ -2,6 +2,7 @@ var http = require('http')
 var nodeunit = require('nodeunit');
 var FeedModel = require('../../src/models/Feed.js');
 var DatabaseDriver = require('../../src/libraries/DatabaseDriver.js');
+var DatabaseMocker = require('../mocks/DatabaseMocker.js');
 var Mongo      = require('mongodb'),
     Db         = Mongo.Db,
     Connection = Mongo.Connection,
@@ -12,52 +13,24 @@ var Mongo      = require('mongodb'),
 exports['save'] = nodeunit.testCase(
 {
 	setUp: function (callback) {
-		/**
-		 * DB Access Parameters
-		 **/
-		var db_name = 'headlyne',
-		    db_addr = '127.0.0.1',
-		    db_port = 27017,
-		    db_user = 'username',
-		    db_pass = 'password';
-
-		DatabaseDriver.init(
-		    db_name,
-		    db_addr,
-		    db_port,
-		    db_user,
-		    db_pass,
-		    function(err)
-		    {
-			    console.log('Suite-setup: '+err.message);
-		    },
-		    function()
-		    {
-			callback();
-		    }
+		DatabaseMocker.setUp(
+			function() {
+				callback();
+			},
+			function(err) {
+				console.log(err);
+			}
 		);
 	},
 	 
 	tearDown: function (callback) {
-		DatabaseDriver.getCollection(
+		DatabaseMocker.tearDown(
 			'feeds',
-			function(err)
-			{
-				console.log('Suite-teardown: '+err.message);
+			function() {
+				callback();
 			},
-			function(collection)
-			{
-				collection.remove(
-					function(err, doc)
-					{
-						if(err != null)
-							console.log('Test-suite cannot terminate.');
-						else {
-							DatabaseDriver.close();
-							callback();
-						}
-					}
-				);
+			function(err) {
+				console.log(err);
 			}
 		);
 	},
