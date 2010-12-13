b9e1469543660dd64c9b03d294d73a1e8eee7dbe
Chetan Surpur <chetan51@gmail.com>
2010-12-12 19:00:04 -0800

    Added FeedServer test file

---
+++ test/libraries/FeedServer.js
@@ -0,0 +1,9 @@
+var http = require('http')
+var nodeunit = require('nodeunit');
+var FeedServer = require('../../src/libraries/FeedServer.js');
+
+exports[''] = nodeunit.testCase(
+{
+	'': function(test) {
+	}
+});
