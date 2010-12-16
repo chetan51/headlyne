fbb72f664ed9dd02925f28d3e66a44367b74d8c0
Chetan Surpur <chetan51@gmail.com>
2010-12-15 21:06:24 -0800

    Cleaned up test for UserAuth (but still not passing)

--- test/libraries/userauth.js
+++ test/libraries/userauth.js
@@ -1,57 +1,35 @@
 var nodeunit = require('nodeunit');
 var DatabaseDriver = require('../../src/libraries/DatabaseDriver');
+var DatabaseFaker = require('../mocks/DatabaseFaker.js');
 var UserModel = require('../../src/models/User')
 var UserAuth = require('../../src/libraries/UserAuth');
 
 exports['authenticate'] = nodeunit.testCase(
 {
 	setUp: function (callback) {
-		/**
-		 * DB Access Parameters
-		 **/
-		var db_name = 'headlyne',
-		    db_addr = '127.0.0.1',
-		    db_port = 27017,
-		    db_user = 'username',
-		    db_pass = 'password';
-
-		DatabaseDriver.init(
-		    db_name,
-		    db_addr,
-		    db_port,
-		    db_user,
-		    db_pass,
-		    function(err)
-		    {
-			    console.log('Suite-setup: '+err.message);
-		    },
-		    function()
-		    {
-			    callback();
-		    }
+		DatabaseFaker.setUp(
+			['users'],
+			function(err) {
+				if (err) {
+					throw err;
+				}
+				else {
+					callback();
+				}
+			}
 		);
 	},
 	 
 	tearDown: function (callback) {
-		DatabaseDriver.getCollection(
-			'users',
-			function(err)
-			{
-				console.log('Suite-teardown: '+err);
-			},
-			function(collection)
-			{
-				collection.remove(
-					function(err, doc)
-					{
-						if(err != null)
-							console.log('Test-suite cannot terminate.');
-						else {
-							DatabaseDriver.close();
-							callback();
-						}
-					}
-				);
+		DatabaseFaker.tearDown(
+			['users'],
+			function(err) {
+				if (err) {
+					throw err;
+				}
+				else {
+					callback();
+				}
 			}
 		);
 	},
@@ -64,29 +42,32 @@ exports['authenticate'] = nodeunit.testCase(
 			'first_name',
 			'last_name',
 			'email_id',
-			function(err)
+			function(err, user)
 			{
-				console.log(err.message);
-				test.done();
-			},
-			function(user)
-			{
-				// user saved.
-				UserAuth.authenticate(
-					'username',
-					'password',
-					function(err)
-					{
-						console.log("auth: "+err.message);
-						test.done();
-					},
-					function(is_new_sesh, sesh)
-					{
-						test.ok(is_new_sesh);
-						console.log(sesh);
-						test.done();
-					}
-				);
+				if (err) {
+					console.log(err.message);
+					test.done();
+				}
+				else {
+					// user saved.
+					UserAuth.authenticate(
+						'username',
+						'password',
+						function(err, is_new_sesh, sesh)
+						{
+							if (err) {
+								console.log("auth: "+err.message);
+								test.done();
+							}
+							else {
+								test.ok(is_new_sesh);
+								console.log(sesh);
+								test.done();
+							}
+						},
+						2 * 7 * 24 * 60 * 60 * 1000
+					);
+				}
 			}
 		);
 	}
