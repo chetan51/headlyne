1dd5c35672f509872ad3c8a7f558b48887ffb9b2
Chetan Surpur <chetan51@gmail.com>
2010-12-16 19:50:12 -0800

    Added second test to Content Grabber's readable function

--- test/libraries/contentgrabber.js
+++ test/libraries/contentgrabber.js
@@ -15,9 +15,16 @@ var html='<html><head></head><body><div id="lol">hello</div></body></html>';
  **/
 var sampleDocument = {
 	url        : './test/mocks/mock_app/views/blogpost1.html',
-	title      : " \n\t\t\t\n            \n                Why Node.js Is Totally Awesome \n            \n            Chetan Surpur\n\t\t\t\n\t\t",
+	title      : "Why Node.js Is Totally Awesome \n            \n            Chetan Surpur",
 	first_line : "Three reasons: speed, easability, and reusability.",
 	last_line  : "Sorry about that everyone!"
+};
+
+var sampleDocument2 = {
+	url        : './test/mocks/mock_app/views/blogpost3.html',
+	title      : "A Curious Breach Of Privacy \n            \n            Chetan Surpur",
+	first_line : "I got this email",
+	last_line  : "life will continue to be"
 };
 
 /**
@@ -51,6 +58,29 @@ exports['grab content from page'] = nodeunit.testCase(
 					test.equal(title, sampleDocument.title);
 					test.notEqual(readableHTML.search(sampleDocument.first_line), -1);
 					test.notEqual(readableHTML.search(sampleDocument.last_line), -1);
+					test.done();
+				}
+			}
+		);
+	},
+
+	'basic 2': function(test)
+	{
+		test.expect(3);
+		
+		var html = fs.readFileSync(sampleDocument2.url, 'utf-8');
+		
+		ContentGrabber.readable(
+			html,
+			function(err, title, readableHTML) {
+				if (err) {
+					test.ifError(err);
+					test.done();
+				}
+				else {
+					test.equal(title, sampleDocument2.title);
+					test.notEqual(readableHTML.search(sampleDocument2.first_line), -1);
+					test.notEqual(readableHTML.search(sampleDocument2.last_line), -1);
 					test.done();
 				}
 			}
