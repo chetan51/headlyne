993870dd19db5cef58cdae173a36e74ec4eaa9af
Chetan Surpur <chetan51@gmail.com>
2010-12-25 17:31:25 -0800

    Added feed controller

---
+++ src/controllers/feed.js
@@ -0,0 +1,55 @@
+/*
+ *  FeedController - a controller that handles all feed-related requests.
+ */
+
+/*
+ *  Module dependencies
+ */
+
+var Ni = require('ni');
+var sys = require('sys');
+var jade = require('jade');
+var Step = require('step');
+var qs = require('querystring');
+
+/*
+ *  The home controller
+ */
+
+var FeedController = function() {
+
+	this.index = function(req, res, next) {
+		res.error("No feed selected.");
+	}
+	
+	this.preview = function(req, res, next) {
+		feed_url = "http://feeds.reuters.com/reuters/companyNews?format=xml";
+		if (typeof(feed_url) == "undefined") {
+			res.error("No feed URL provided.");
+		}
+		else {
+			Ni.library('FeedServer').getFeedTeaser(
+				feed_url,
+				1,
+				function() {},
+				function(err, teaser) {
+					if (err) throw err;
+					
+					var preview = jade.render(
+						Ni.view('preview').template,
+						{locals: teaser}
+					);
+
+					res.ok(preview);
+				}
+			);
+		}
+	}
+
+};
+
+/*
+ *  Exports the home controller
+ */
+
+module.exports = new FeedController();
