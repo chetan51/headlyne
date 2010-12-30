546e31340f09cfd282e110751b550c257b12ede7
Chetan Surpur <chetan51@gmail.com>
2010-12-30 00:38:59 -0800

    Progress on feed editing UI

--- src/public/includes/js/client.js
+++ src/public/includes/js/client.js
@@ -32,8 +32,8 @@ $(document).ready(function() {
 	$("#expand-button").click(expandOrCollapseClicked);
 	$("#collapse-button").click(expandOrCollapseClicked);
 	
-	$("#edit-button").click(editOrDoneClicked);
-	$("#done-button").click(editOrDoneClicked);
+	$("#edit-button").click(editClicked);
+	$("#done-button").click(doneClicked);
 	
 	$("#add-column-button").click(addColumnClicked);
 	
@@ -48,24 +48,34 @@ function expandOrCollapseClicked(e) {
 	$("#collapse-control").toggle();
 }
 
+function editClicked(e) {
+	editOrDoneClicked(e);
+}
+
+function doneClicked(e) {
+	$(".feed-preview").hide("slide", {direction: "up"}, "fast");
+	editOrDoneClicked(e);
+}
+
 function editOrDoneClicked(e) {
 	$(".feed-body").slideToggle("fast");
 	$(".feed-header").slideToggle("fast");
 	
 	$("#edit-editing-control").toggle();
 	$("#edit-default-control").toggle();
+	
+	equallyWidenColumns();
 }
 
 function addColumnClicked(e) {
 	var new_column = $("<div></div>").addClass("column ui-sortable");
 	$(".page").append(new_column);
 	
-	// Resize all the columns
-	var width = 100 / $(".column").length;
-	$(".column").css("width", width + "%");
+	equallyWidenColumns();
 }
 
 function feedEditClicked(e) {
+	var this_column = $(this).parents(".column");
 	var preview_container = $(this).parents(".feed").children(".feed-preview");
 	var url_container = $(this).parents(".feed").children(".feed-url");
 	
@@ -75,10 +85,16 @@ function feedEditClicked(e) {
 	$.ajax({
 		url: "/feed/preview",
 		data: {
-			url: url_container.text(),
+			url: url_container.text()
 		},
 		success: function(data) {
 			preview_container.html(data);
+			
+			// Resize all columns based on preview width
+			var this_column_width_percent = 50;
+			var other_columns_width_percent = (100 -this_column_width_percent) / ($(".column").length - 1);
+			$(".column").css("width", other_columns_width_percent+"%");
+			this_column.css("width", this_column_width_percent+"%");
 		},
 		error: function() {
 			preview_container.html("An error was encountered.");
@@ -94,3 +110,12 @@ function feedHeaderHoverIn(e) {
 function feedHeaderHoverOut(e) {
 	$(this).children(".feed-edit-overlay").hide();
 }	
+
+
+/*
+ * Helper functions
+ */
+function equallyWidenColumns() {
+	var width = 100 / $(".column").length;
+	$(".column").css("width", width + "%");
+}
