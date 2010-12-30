2ca192a3127c8b7ebf84372110d59e75fec61026
Vanwaril <manojm@ieee.org>
2010-12-30 23:02:05 +0530

    Barebones add-feed controller function.

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -46,6 +46,69 @@ var FeedController = function() {
 		}
 	}
 
+	this.add = function(req, res, next, row, column, feed_url) {
+		// if no cookies are passed, redirect to login.
+		if( typeof(req.headers.cookie) == 'undefined' )
+		{
+			res.writeHead(302, [
+				['Location', '/login']
+			]);
+			res.end();
+			return;
+		}
+		// check if the cookie is a JSON object. otherwise, say cookie error.
+		var cookie;
+		try {
+			cookie = JSON.parse(req.headers.cookie);
+			console.log(cookie);
+		} catch (e) {
+			res.error('Your cookie is broken. Please clear cookies '+
+				'for this site and try again');
+			return;
+		}
+
+		// check if the cookie is valid.
+		Ni.library('UserAuth').checkAuth(
+			cookie,
+			function(err, is_valid)
+			{
+				if(err) {
+					console.log(err.message);
+					res.writeHead(302, [
+						['Location', '/logout'],
+					]);
+					res.end();
+					return;
+				}
+				if(!is_valid) {
+					res.writeHead(302, [
+						['Location', '/login']
+					]); // redirect to login page.
+					res.end();
+				} else {
+					// if valid, serve the page requested.
+					
+					Ni.model('User').addFeed(
+						cookie.data.user,
+						feed_url,
+						row,
+						column,
+						function(err, feeds)
+						{
+							if(err) {
+								res.ok({'Error': 'Cannot add feed'});
+								console.log('cannot add feed: '+err.message);
+							} else {
+								res.ok(feeds);
+								console.log(feeds);
+							}
+						}
+					);
+				}
+			}
+		);
+	}
+
 };
 
 /*
