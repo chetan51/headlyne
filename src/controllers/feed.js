edfeffd3ff71ac9904cbba2ddb0d46e19cc2e310
Vanwaril <manojm@ieee.org>
2011-01-01 01:12:31 +0530

    Updated /feed/preview -- accepts POST'd argument, returns JSON object.

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -19,30 +19,80 @@ var FeedController = function()
 	{
 		res.error("No feed selected.");
 	}
-	
+
+	// temporary location for function -- should be globally usable!!
+	function getPOST(req, callback)
+	{
+		var returned = false;
+		if( req.method == 'POST') {
+			req.addListener('data', function(chunk)
+			{
+				try{
+					POST = querystring.parse(chunk);
+				} catch(e) {
+					returned = true;
+					callback(e);
+				}
+			});
+			req.addListener('end', function()
+			{
+				if(!returned)
+					callback(null, POST);
+			});
+		} else callback(new Error('No POST data'));
+	}
+
 	this.preview = function(req, res, next)
 	{
-		feed_url = "http://feeds.reuters.com/reuters/companyNews?format=xml";
-		if (typeof(feed_url) == "undefined") {
-			res.error("No feed URL provided.");
-		} else {
+		// object to be filled and returned.
+		var res_obj = {
+			'error': null,
+			'preview': ''
+		};
+
+		// get POST variables, then proceed.
+		getPOST(req, function(err, POST)
+		{
+			// check if there are any errors...
+			if(err) {
+				res_obj.error = err;
+				res.ok(res_obj);
+				return;
+			}
+			
+			if(typeof(POST.feed_url) == 'undefined') {
+				res_obj.error = new Error('No feed provided');
+				res.ok(res_obj);
+				return;
+			}
+		
+			// STUBBED
+			POST.feed_url = "http://feeds.reuters.com/reuters/companyNews?format=xml";
+
+			// now get the feed teaser
 			Ni.library('FeedServer').getFeedTeaser(
-				feed_url,
+				POST.feed_url,
 				1,
 				function() {},
 				function(err, teaser)
 				{
-					if (err) throw err;
+					if (err) {
+						res_obj.error = err;
+						res.ok(res_obj);
+						return;
+					}
 					
+					// render the preview and return it.
 					var preview = jade.render(
 						Ni.view('preview').template,
 						{locals: teaser}
 					);
 
-					res.ok(preview);
+					res_obj.preview = preview;
+					res.ok(res_obj);
 				}
 			);
-		}
+		});
 	}
 };
 
