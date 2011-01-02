e53c66d76fa6234bfcbf933c8179ed0f78acc0e9
Chetan Surpur <chetan51@gmail.com>
2011-01-01 18:40:00 -0800

    Fixed some animations

--- src/public/includes/js/client.js
+++ src/public/includes/js/client.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
 	
 	var edit_overlays = header_containers.children(".edit-overlay");
 	edit_overlays.hide();
-	edit_overlays.find(".edit-delete > .delete > .deleting-control").hide();
-	edit_overlays.find(".edit-delete > .edit > .editing-control").hide();
+	edit_overlays.find("> .edit-delete > .delete > .deleting-control").hide();
+	edit_overlays.find("> .edit-delete > .edit > .editing-control").hide();
 	
 	var column_containers = $(".column");
 	var header_containers = column_containers.children(".header");
@@ -24,7 +24,7 @@ $(document).ready(function() {
 	
 	var edit_overlays = header_containers.children(".edit-overlay");
 	edit_overlays.hide();
-	edit_overlays.find(".delete > .deleting-control").hide();
+	edit_overlays.find("> .delete > .deleting-control").hide();
 	
 	// Set up overlays
 	var triggers = $(".modalInput").overlay({
@@ -62,26 +62,25 @@ function addColumnListeners(columns) {
 		handle: $(".feed > .header")
 	});
 	
-	var delete_container = columns.find(".header > .edit-overlay > .delete"); 
-	delete_container.find(".default-control > .delete-button").click(columnDeleteClicked);
-	delete_container.find(".deleting-control > .cancel-button").click(columnDeleteCancelClicked);
-	columns.find(".confirm-delete > .delete-button").click(columnDeleteConfirmClicked);
+	var delete_container = columns.find("> .header > .edit-overlay > .delete"); 
+	delete_container.find("> .default-control > .delete-button").click(columnDeleteClicked);
+	delete_container.find("> .deleting-control > .cancel-button").click(columnDeleteCancelClicked);
+	columns.find("> .confirm-delete > .delete-button").click(columnDeleteConfirmClicked);
 	
 	columns.hover(columnHoverIn, columnHoverOut);
 }
 
 function addFeedListeners(feeds) {
-	var edit_delete_container = feeds.find(".header > .edit-overlay > .edit-delete"); 
+	var edit_delete_container = feeds.find("> .header > .edit-overlay > .edit-delete"); 
 
 	var edit_container = edit_delete_container.children(".edit");
-	edit_container.find(".default-control > .edit-button").click(feedEditClicked);
-	edit_container.find(".editing-control > .done-button").click(feedDoneClicked);
-	edit_container.find(".editing-control > .done-button").click(feedDoneClicked);
+	edit_container.find("> .default-control > .edit-button").click(feedEditClicked);
+	edit_container.find("> .editing-control > .done-button").click(feedDoneClicked);
 	
 	var delete_container = edit_delete_container.children(".delete");
-	delete_container.find(".default-control > .delete-button").click(feedDeleteClicked);
-	delete_container.find(".deleting-control > .cancel-button").click(feedDeleteCancelClicked);
-	feeds.find(".confirm-delete > .delete-button").click(feedDeleteConfirmClicked);
+	delete_container.find("> .default-control > .delete-button").click(feedDeleteClicked);
+	delete_container.find("> .deleting-control > .cancel-button").click(feedDeleteCancelClicked);
+	feeds.find("> .confirm-delete > .delete-button").click(feedDeleteConfirmClicked);
 	
 	feeds.children(".header").hover(feedHeaderHoverIn, feedHeaderHoverOut);
 }
@@ -104,7 +103,7 @@ function editClicked(e) {
 function doneClicked(e) {
 	hideFeedPreviews();
 	
-	var edit_containers = $(".feed").find(".header > .edit-overlay > .edit-delete > .edit");
+	var edit_containers = $(".feed").find("> .header > .edit-overlay > .edit-delete > .edit");
 	edit_containers.children(".default-control").show();
 	edit_containers.children(".editing-control").hide();
 	
@@ -143,58 +142,58 @@ function addColumnClicked(e) {
 function feedEditClicked(e) {
 	var this_column = $(this).parents(".column");
 	var preview_container = $(this).parents(".feed").children(".preview");
-	var settings_container = $(this).parents(".feed").find(".header > .settings");
+	var settings_container = $(this).parents(".feed").find("> .header > .settings");
 	var feed_url = settings_container.children(".url").text();
 	var title_selection = settings_container.children(".title-selection").text();
 	var body_selection = settings_container.children(".body-selection").text();
 	
-	var edit_container = $(this).parents(".feed").find(".header > .edit-overlay > .edit-delete > .edit");
+	var edit_container = $(this).parents(".feed").find("> .header > .edit-overlay > .edit-delete > .edit");
 	edit_container.children(".default-control").hide();
 	edit_container.children(".editing-control").show();
 	
 	preview_container.html("Loading feed preview...");
-	preview_container.slideDown("fast");
-	
-	$.ajax({
-		url: "/feed/preview",
-		data: {
-			url: feed_url
-		},
-		success: function(data) {
-			preview_container.hide();
-			preview_container.html(data);
-			preview_container.slideDown("fast");
-			
-			resizeColumnDynamically(this_column, 50);
-			
-			// Mark selected settings
-			var titles_form = preview_container.find(".display > .titles > form");
-			var bodies_form = preview_container.find(".display > .bodies > form");
-			if (title_selection == "item") {
-				titles_form.find(".item > .control > .input").click();
+	preview_container.slideDown("fast", function() {
+		$.ajax({
+			url: "/feed/preview",
+			data: {
+				url: feed_url
+			},
+			success: function(data) {
+				preview_container.hide();
+				preview_container.html(data);
+				preview_container.slideDown("fast");
+				
+				resizeColumnDynamically(this_column, 50);
+				
+				// Mark selected settings
+				var titles_form = preview_container.find("> .display > .titles > form");
+				var bodies_form = preview_container.find("> .display > .bodies > form");
+				if (title_selection == "item") {
+					titles_form.find("> .item > .control > .input").click();
+				}
+				else if (title_selection == "webpage") {
+					titles_form.find("> .webpage > .control > .input").click();
+				}
+				
+				if (body_selection == "item") {
+					bodies_form.find("> .item > .control > .input").click();
+				}
+				else if (body_selection == "webpage") {
+					bodies_form.find("> .webpage > .control > .input").click();
+				}
+			},
+			error: function() {
+				// Test if this is hit when server is off
+				preview_container.hide();
+				preview_container.html("An error was encountered.");
+				preview_container.slideDown("fast");
 			}
-			else if (title_selection == "webpage") {
-				titles_form.find(".webpage > .control > .input").click();
-			}
-			
-			if (body_selection == "item") {
-				bodies_form.find(".item > .control > .input").click();
-			}
-			else if (body_selection == "webpage") {
-				bodies_form.find(".webpage > .control > .input").click();
-			}
-		},
-		error: function() {
-			// Test if this is hit when server is off
-			preview_container.hide();
-			preview_container.html("An error was encountered.");
-			preview_container.slideDown("fast");
-		}
+		});
 	});
 }
 
 function feedDoneClicked(e) {
-	var edit_container = $(this).parents(".feed").find(".header > .edit-overlay > .edit-delete > .edit");
+	var edit_container = $(this).parents(".feed").find("> .header > .edit-overlay > .edit-delete > .edit");
 	edit_container.children(".default-control").show();
 	edit_container.children(".editing-control").hide();
 	
@@ -206,7 +205,7 @@ function feedDeleteClicked(e) {
 	var feed_container = $(this).parents(".feed");
 	feed_container.children(".confirm-delete").slideDown("fast");
 	
-	var delete_container = feed_container.find(".header > .edit-overlay > .edit-delete > .delete");
+	var delete_container = feed_container.find("> .header > .edit-overlay > .edit-delete > .delete");
 	delete_container.children(".default-control").hide();
 	delete_container.children(".deleting-control").show();
 }
@@ -215,7 +214,7 @@ function feedDeleteCancelClicked(e) {
 	var feed_container = $(this).parents(".feed");
 	feed_container.children(".confirm-delete").slideUp("fast");
 	
-	var delete_container = feed_container.find(".header > .edit-overlay > .edit-delete > .delete");
+	var delete_container = feed_container.find("> .header > .edit-overlay > .edit-delete > .delete");
 	delete_container.children(".default-control").show();
 	delete_container.children(".deleting-control").hide();
 }
@@ -237,7 +236,7 @@ function columnDeleteClicked(e) {
 	var column_container = $(this).parents(".column");
 	column_container.children(".confirm-delete").slideDown("fast");
 	
-	var delete_container = column_container.find(".header > .edit-overlay > .delete");
+	var delete_container = column_container.find("> .header > .edit-overlay > .delete");
 	delete_container.children(".default-control").hide();
 	delete_container.children(".deleting-control").show();
 }
@@ -246,7 +245,7 @@ function columnDeleteCancelClicked(e) {
 	var column_container = $(this).parents(".column");
 	column_container.children(".confirm-delete").slideUp("fast");
 	
-	var delete_container = column_container.find(".header > .edit-overlay > .delete");
+	var delete_container = column_container.find("> .header > .edit-overlay > .delete");
 	delete_container.children(".default-control").show();
 	delete_container.children(".deleting-control").hide();
 }
@@ -269,12 +268,12 @@ function columnHoverOut(e) {
  * Helper functions
  */
 function equallyWidenColumns() {
-	var width = 100 / $(".column").length;
+	var width = 99 / $(".column").length;
 	$(".column").animate({"width" : width + "%"}, "fast");
 }
 
 function resizeColumnDynamically(column, width_percent) {
-	var other_columns_width_percent = (100 - width_percent) / ($(".column").length - 1);
+	var other_columns_width_percent = (99 - width_percent) / ($(".column").length - 1);
 	
 	column.animate({"width" : width_percent+"%"}, "fast", function() {
 		if (width_percent == 0) {
