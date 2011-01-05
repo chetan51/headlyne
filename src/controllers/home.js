357d8fb74310b68f2c37aec2d8a4e0d630bcead8
Chetan Surpur <chetan51@gmail.com>
2011-01-04 17:46:09 -0800

    Cleaned up some redirection

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -235,13 +235,7 @@ var HomeController = function()
 				dbg.log(err.message);
 				
 				res.clearCookie('cookie');
-				res.writeHead( 302, {
-					Location: '/home/login'
-				});
-				res.end();
-							/*res.writeHead( 200 );
-							res.write('logged out');
-							res.end();*/
+				res.moved('/home/login');
 			} else {
 				dbg.log(cookie.data);
 				Ni.library('UserAuth').invalidate(
@@ -251,11 +245,8 @@ var HomeController = function()
 							// no errors -- attach a null cookie, direct to
 							// login page, and get moving.
 							res.clearCookie('cookie');
-							res.writeHead( 302, {
-								Location: '/home/login'
-							});
+							res.moved('/home/login');
 							dbg.log('redirect: logout to login.');
-							res.end();
 						}
 				);
 			}
