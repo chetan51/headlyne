a1c4c9f255b78ab1af3fc272bf7c4fddc3a33890
Chetan Surpur <chetan51@gmail.com>
2011-01-10 20:26:45 -0800

    Removed unnecessary function

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -83,68 +83,6 @@ var HomeController = function()
 	}
 	
 	/*
-	 *	Helper that creates teaser according to given user's specifications.
-	 *	
-	 *		Arguments: user
-	 *		           feed URL
-	 *		
-	 *		Returns (via callback): error
-	 *		                        teaser in JSON format
-	 */
-	this._createTeaser = function(user, feed_url, callback)
-	{
-		var global_feed;
-		Step(
-			function findFeed()
-			{
-				dbg.log('find feed'); 
-				Ni.model('User').getFeed(
-					user.username,
-					feed_url,
-					this
-				);
-			},
-			function generateTeaser(err, feed)
-			{
-				global_feed = feed;
-				if(err) throw err; // rethrows error
-
-				dbg.log('gen teaser'); 
-				Ni.library('FeedServer').getFeedTeaser(
-					feed_url,
-					feed.num_feed_items,
-					function(){},
-					this
-				);
-			},
-			function updateTeaser(err, teaser)
-			{
-				dbg.log('Err: '+err+'. update teaser...');
-				if(err) throw err;
-				for( keys in global_feed ) {
-					dbg.log('key '+keys);
-					teaser[keys] = global_feed[keys];
-				}
-				return teaser;
-			},
-			function genPage(err, feed)
-			{
-				dbg.log('Err: '+err+'. genpage...');
-				if(err) throw err;
-				var teaser = Ni.library('Templater').getFeedTeaser(
-					{feed: feed}
-				);
-				return teaser;
-			},
-			function fireCallback(err, teaser)
-			{
-				dbg.log('returning teaser. Err: '+err);
-				callback(err, teaser);
-			}
-		);
-	}
-	
-	/*
 	 *	Helper that generates columns of feeds according to given feed map.
 	 *	
 	 *		Arguments: feed map
