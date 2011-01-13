8a26d507cf2aaae715adb15d9b226060d13fe8de
Chetan Surpur <chetan51@gmail.com>
2011-01-12 18:11:33 -0800

    Added debugger called function for stack trace logging

--- src/libraries/Debugger.js
+++ src/libraries/Debugger.js
@@ -10,10 +10,21 @@ var Debugger = function() {
 	
 	var self = this;
 	
+	this.called = function called(owner_class) {
+		if (Ni.config('log_enabled')) {
+			var owner_label = "";
+			if (owner_class) {
+				owner_label = owner_class + ".";
+			}
+
+			console.log("Called: " + owner_label + arguments.callee.caller.name);
+		}
+	}
+	
 	this.log = function log(message)
 	{
 		if (Ni.config('log_enabled')) {
-			console.log(message);
+			console.log(message + "[" + arguments.callee.caller.name + "]");
 		}
 	}
 };
