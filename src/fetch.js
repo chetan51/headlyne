788f4bad0d758df50de8a613550c33cad7d6ecfd
Chetan Surpur <chetan51@gmail.com>
2011-01-24 20:10:53 -0800

    Migrated Downloader's fetch to resque

---
+++ src/fetch.js
@@ -0,0 +1,157 @@
+/**
+ * 
+ **/
+
+/**
+ *  Module dependencies
+ **/
+var Connect = require('connect'),
+    Quip    = require('quip'),
+    Ni      = require('ni');
+    dbg     = require('./libraries/Debugger.js');
+
+var resque = require('coffee-resque').connect({
+	host: "localhost",
+	port: 6379
+});
+
+/**
+ * Constants
+ **/
+var db_name = 'headlyne',
+    db_addr = '127.0.0.1',
+    db_port = 27017,
+    db_user = 'username',
+    db_pass = 'password';
+
+/**
+ *  Load Ni and start the server.
+ **/
+Ni.config('root', __dirname);
+
+/**
+ *  App configuration
+ **/
+
+Ni.config('base_url',            "/");
+Ni.config('http_timeout',        30000);
+Ni.config('feedparse_timeout',   5000);
+Ni.config('feed_expiry_length',  30 * 60 * 1000);
+Ni.config('max_redirect',        5);
+Ni.config('session_lifetime',    14 * 24 * 60 * 60 * 1000);
+Ni.config('snippet_image_limit', 2);
+Ni.config('snippet_text_limit',  300);
+Ni.config('log_enabled',         true);
+
+Ni.config('default_feeds',
+	[
+		[
+			{
+				url: 'http://feeds.feedburner.com/quotationspage/qotd',
+				num_feed_items: 9,
+				body_selection: 'item',
+				title_selection: 'item'
+			}
+		],
+		[
+			{
+				url: 'http://feeds.reuters.com/reuters/companyNews?format=xml',
+				num_feed_items: 2,
+				body_selection: 'webpage',
+				title_selection: 'webpage'
+			},
+			{
+				url: 'http://feeds.reuters.com/reuters/entertainment',
+				num_feed_items: 2,
+				body_selection: 'item',
+				title_selection: 'webpage'
+			},
+		],
+		[
+			{
+				url: 'http://feeds.feedburner.com/FutilityCloset',
+				num_feed_items: 3,
+				body_selection: 'webpage',
+				title_selection: 'item'
+			}
+		]
+	]
+);		
+
+Ni.boot(function initializeDatabase() {
+	dbg.called();
+	
+	Ni.library('DatabaseDriver').init(
+		db_name,
+		db_addr,
+		db_port,
+		db_user,
+		db_pass,
+		function startServer(err) {
+			dbg.called();
+			
+			/*
+			Ni.library('DatabaseDriver').getCollection(
+				'feeds',
+				function(err, collection)
+				{
+					if (err) {
+						throw err;
+					}
+					else {
+						collection.remove(
+							function(err, doc)
+							{
+								if(err != null)
+									throw err;
+								else {
+								}
+							}
+						);
+					}
+				}
+			);
+			
+			Ni.library('DatabaseDriver').getCollection(
+				'webpages',
+				function(err, collection)
+				{
+					if (err) {
+						throw err;
+					}
+					else {
+						collection.remove(
+							function(err, doc)
+							{
+								if(err != null)
+									throw err;
+								else {
+								}
+							}
+						);
+					}
+				}
+			);
+			*/
+			
+			if (err) throw err;
+			
+			/*
+			Ni.model('Invite').add(
+				'admin',
+				function(err, invite_code) {
+					if (err) throw err;
+
+					console.log(invite_code);
+				}
+			);
+			*/
+			
+			// Connect workers
+			for (var i = 0; i < 5; i++) {
+				var worker = resque.worker('Downloader', Ni.library('Downloader').worker);
+				worker.start();
+			}
+		}
+	);
+});
--- src/libraries/Downloader.js
+++ src/libraries/Downloader.js
@@ -7,6 +7,11 @@ var u    = require('url'),
     Ni   = require('ni'),
     dbg  = require('./Debugger');
 
+var resque = require('coffee-resque').connect({
+	host: "localhost",
+	port: 6379
+});
+
 /**
  *	Downloader library
  **/
@@ -17,72 +22,89 @@ var Downloader = function() {
 	this.fetch = function fetch(url, callback)
 	{
 		dbg.called();
-		dbg.log("Fetching url: " + url);
 		
-		if (url.length > 0) {
-			self.fetch_helper(url, Ni.config('max_redirect'), callback);
-		}
-		else {
-			callback(new Error('Invalid URL.'));
-		}
+		resque.enqueue(
+			'Downloader',
+			'fetch',
+			[url],
+			callback
+		);
 	}
 	
-	this.fetch_helper = function fetch_helper(url, max_redirect_level, callback)
+	this.worker = new function()
 	{
-		dbg.called();
+		var self = this;
 		
-		if(max_redirect_level == 0) {
-			callback(new Error('Endless redirection.'));
-			return;
+		this.fetch = function fetch(url, callback)
+		{
+			dbg.called();
+			dbg.log("Fetching url: " + url);
+			
+			if (url.length > 0) {
+				self.fetch_helper(url, Ni.config('max_redirect'), callback);
+			}
+			else {
+				callback(new Error('Invalid URL.'));
+			}
 		}
 		
-		var timeout = setTimeout(function timedOut() {
+		this.fetch_helper = function fetch_helper(url, max_redirect_level, callback)
+		{
 			dbg.called();
-		
-			callback(new Error('Request timed out.'));
-		}, Ni.config('http_timeout'));
-		
-		var options = {
-			followRedirects: false, // we implement our own redirection following, since
-			                        // Restler cannot handle infinite redirection
-			parser: false
-		};
-
-		rest.get(url, options).addListener('complete', function processResponse(data, response) {
-			dbg.called();
-		
-			clearTimeout(timeout);
 			
-			switch(response.statusCode) {
-				case 200:
-					callback(null, data);
-					break;
-				case 301:
-				case 302:
-					self.fetch_helper(
-						response.headers.location,
-						max_redirect_level-1,
-						callback
-					);
-					break;
-				default:
-					callback(new Error("Error " + response.statusCode + ": Page not found."));
-					break;
+			if(max_redirect_level == 0) {
+				callback(new Error('Endless redirection.'));
+				return;
 			}
-		}).addListener('error', function processError(data, response) {
-			dbg.called();
-			dbg.log('download error: ' + response);
 			
-			clearTimeout(timeout);
-			callback(new Error("Error :" + response));
-		});
+			var timeout = setTimeout(function timedOut() {
+				dbg.called();
+			
+				callback(new Error('Request timed out.'));
+			}, Ni.config('http_timeout'));
+			
+			var options = {
+				followRedirects: false, // we implement our own redirection following, since
+								// Restler cannot handle infinite redirection
+				parser: false
+			};
 
-		process.on('uncaughtException', function processUncaughtException(err){
-			dbg.called();
-		
-			dbg.error(err);
-			// allow request to time out.
-		});
+			rest.get(url, options).addListener('complete', function processResponse(data, response) {
+				dbg.called();
+			
+				clearTimeout(timeout);
+				
+				switch(response.statusCode) {
+					case 200:
+						callback(null, data);
+						break;
+					case 301:
+					case 302:
+						self.fetch_helper(
+							response.headers.location,
+							max_redirect_level-1,
+							callback
+						);
+						break;
+					default:
+						callback(new Error("Error " + response.statusCode + ": Page not found."));
+						break;
+				}
+			}).addListener('error', function processError(data, response) {
+				dbg.called();
+				dbg.log('download error: ' + response);
+				
+				clearTimeout(timeout);
+				callback(new Error("Error :" + response));
+			});
+
+			process.on('uncaughtException', function processUncaughtException(err){
+				dbg.called();
+			
+				dbg.error(err);
+				// allow request to time out.
+			});
+		}
 	}
 };
 
